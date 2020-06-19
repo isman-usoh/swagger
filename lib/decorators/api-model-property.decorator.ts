01328bf0607d3cd4ad462e0f5c6da319a7b36fc3
Isman Usoh <isman.usoh@gmail.com>
2020-06-19 12:40:08 +0700

    add: additionalProperties

--- lib/decorators/api-model-property.decorator.ts
+++ lib/decorators/api-model-property.decorator.ts
@@ -2,34 +2,86 @@ import { DECORATORS } from '../constants';
 import { SwaggerEnumType } from '../types/swagger-enum.type';
 import { createPropertyDecorator, getTypeIsArrayTuple } from './helpers';
 
+export interface ReferenceObject {
+  $ref: string;
+}
+
+export interface SchemaObject {
+  additionalProperties?: SchemaObject | ReferenceObject | boolean;
+  allOf?: (SchemaObject | ReferenceObject)[];
+  anyOf?: (SchemaObject | ReferenceObject)[];
+  collectionFormat?: string;
+  default?: any;
+  deprecated?: boolean;
+  description?: string;
+  enum?: any[];
+  example?: any;
+  examples?: any[];
+  exclusiveMaximum?: boolean;
+  exclusiveMinimum?: boolean;
+  format?: string;
+  items?: SchemaObject | ReferenceObject;
+  maxItems?: number;
+  maxLength?: number;
+  maxProperties?: number;
+  maximum?: number;
+  minItems?: number;
+  minLength?: number;
+  minProperties?: number;
+  minimum?: number;
+  multipleOf?: number;
+  not?: SchemaObject | ReferenceObject;
+  nullable?: boolean;
+  oneOf?: (SchemaObject | ReferenceObject)[];
+  pattern?: string;
+  properties?: Record<string, SchemaObject | ReferenceObject>;
+  readOnly?: boolean;
+  required?: string[];
+  title?: string;
+  type?: string;
+  uniqueItems?: boolean;
+  writeOnly?: boolean;
+  xml?: any;
+}
+
+export interface SchemaObjectMetadata
+  extends Omit<SchemaObject, 'type' | 'required'> {
+  type?: any;
+  isArray?: boolean;
+  required?: boolean;
+  name?: string;
+  enumName?: string;
+}
+
 export const ApiModelProperty = (
   metadata: {
-    description?: string;
-    required?: boolean;
-    type?: any;
-    isArray?: boolean;
+    additionalProperties?: SchemaObject | ReferenceObject | boolean;
     collectionFormat?: string;
     default?: any;
+    description?: string;
     enum?: SwaggerEnumType;
+    example?: any;
+    exclusiveMaximum?: boolean;
+    exclusiveMinimum?: boolean;
     format?: string;
     in?: string;
-    multipleOf?: number;
-    maximum?: number;
-    exclusiveMaximum?: boolean;
-    minimum?: number;
-    exclusiveMinimum?: boolean;
-    maxLength?: number;
-    minLength?: number;
-    pattern?: string;
+    isArray?: boolean;
     maxItems?: number;
-    minItems?: number;
-    uniqueItems?: boolean;
+    maxLength?: number;
     maxProperties?: number;
+    maximum?: number;
+    minItems?: number;
+    minLength?: number;
     minProperties?: number;
-    readOnly?: boolean;
+    minimum?: number;
+    multipleOf?: number;
     nullable?: boolean;
+    pattern?: string;
+    readOnly?: boolean;
+    required?: boolean;
+    type?: any;
+    uniqueItems?: boolean;
     xml?: any;
-    example?: any;
   } = {}
 ): PropertyDecorator => {
   const [type, isArray] = getTypeIsArrayTuple(metadata.type, metadata.isArray);
@@ -42,31 +94,32 @@ export const ApiModelProperty = (
 
 export const ApiModelPropertyOptional = (
   metadata: {
-    description?: string;
-    type?: any;
-    isArray?: boolean;
+    additionalProperties?: SchemaObject | ReferenceObject | boolean;
     collectionFormat?: string;
     default?: any;
+    description?: string;
     enum?: SwaggerEnumType;
+    example?: any;
+    exclusiveMaximum?: boolean;
+    exclusiveMinimum?: boolean;
     format?: string;
     in?: string;
-    multipleOf?: number;
-    maximum?: number;
-    exclusiveMaximum?: boolean;
-    minimum?: number;
-    exclusiveMinimum?: boolean;
-    maxLength?: number;
-    minLength?: number;
-    pattern?: string;
+    isArray?: boolean;
     maxItems?: number;
-    minItems?: number;
-    uniqueItems?: boolean;
+    maxLength?: number;
     maxProperties?: number;
+    maximum?: number;
+    minItems?: number;
+    minLength?: number;
     minProperties?: number;
-    readOnly?: boolean;
+    minimum?: number;
+    multipleOf?: number;
     nullable?: boolean;
+    pattern?: string;
+    readOnly?: boolean;
+    type?: any;
+    uniqueItems?: boolean;
     xml?: any;
-    example?: any;
   } = {}
 ): PropertyDecorator =>
   ApiModelProperty({
